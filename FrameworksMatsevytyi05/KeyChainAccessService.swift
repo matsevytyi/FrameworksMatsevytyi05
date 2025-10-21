b5dd61ab960b498b25303c7e715c86460c056a47
Andrii Matsevytyi <macevitiya@i.ua>
2025-10-21 22:05:40 +0300

    moved keychain auth to separate service, modified logic

---
+++ FrameworksMatsevytyi05/KeyChainAccessService.swift
@@ -0,0 +1,47 @@
+import Security
+import Foundation
+
+class KeyChainAccessService {
+    static let shared = KeyChainAccessService()
+
+    func save(key: String, value: String) {
+        guard let data = value.data(using: .utf8) else { return }
+
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecValueData as String: data
+        ]
+
+        SecItemDelete(query as CFDictionary)
+        SecItemAdd(query as CFDictionary, nil)
+        
+    }
+
+    func get(key: String) -> String? {
+        
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+
+        var dataTypeRef: AnyObject?
+        
+        if SecItemCopyMatching(query as CFDictionary, &dataTypeRef) == noErr,
+           let data = dataTypeRef as? Data {
+            return String(data: data, encoding: .utf8)
+        }
+        
+        return nil
+    }
+
+    func delete(key: String) {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key
+        ]
+        SecItemDelete(query as CFDictionary)
+    }
+}
